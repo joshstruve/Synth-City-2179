6d4da44e1afe61a9ec4be7c4e42cf8d3d4a93ee5
Josh Struve <63620595+joshstruve@users.noreply.github.com>
2020-08-15 15:02:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -142,6 +142,21 @@ function onMouseMove(event) {
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
+function onDocumentTouchStart( event ) {
+  if ( event.touches.length == 1 ) {
+    event.preventDefault();
+    mouse.x = event.touches[ 0 ].pageX -  window.innerWidth / 2;
+    mouse.y = event.touches[ 0 ].pageY - window.innerHeight / 2;
+  }
+}
+function onDocumentTouchMove( event ) {
+  if ( event.touches.length == 1 ) {
+    event.preventDefault();
+    mouse.x = event.touches[ 0 ].pageX -  window.innerWidth / 2;
+    mouse.y = event.touches[ 0 ].pageY - window.innerHeight / 2;
+  }
+}
+
 window.addEventListener('mousemove', onMouseMove, false);
 window.addEventListener('touchstart', onDocumentTouchStart, false );
 window.addEventListener('touchmove', onDocumentTouchMove, false );
